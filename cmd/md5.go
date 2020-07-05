ccb7c37dc72563f0edea8837788f26a2e1f41d92
João Pedro Alvarenga <joaopedroaats@pm.me>
2020-07-05 11:44:52 -0300

    initial commit

---
+++ cmd/md5.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// md5Cmd represents the md5 command
+var md5Cmd = &cobra.Command{
+	Use:   "md5",
+	Short: "Security: Very low | Encryption or fingerprint function for a file.",
+	Long:  `The MD5 message-digest algorithm is a widely used hash function producing a 128-bit hash value. Although MD5 was initially designed to be used as a cryptographic hash function, it has been found to suffer from extensive vulnerabilities.`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("md5 called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(md5Cmd)
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// md5Cmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// md5Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
